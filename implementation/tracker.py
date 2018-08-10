53a5d01a2a5630cfbb74cfaca30ce8d24291855e
juliakamelina <juliak.d@mail.ru>
2018-08-10 21:32:47 +0300

    img

--- implementation/tracker.py
+++ implementation/tracker.py
@@ -1,5 +1,10 @@
+import numpy as np
+import cv2 as cv
+import math
+
 def tracker(**params):
     #Get sequence info
+    # TODO: when load image use from scipy.ndimage import imread
     seq, im = get_sequence_info(params["seq"])
     del params["seq"]
     if not im:
@@ -38,3 +43,30 @@ def tracker(**params):
     global_fparams["data_type"] = params["data_type"]
 
     init_target_sz = target_sz
+
+    # Check if colour image
+    shape = im.shape
+    if shape[2] == 3:
+        is_color_image = True
+    else:
+        is_color_image = False
+        
+    # Check if mexResize available
+    # TODO:
+
+    # Calculate search area and initial scale factor
+    search_area = np.prod(np.array(init_target_sz, float) * params["search_area_scale"])
+    if search_area > params["max_image_sample_size"]:
+        currentScaleFactor = sqrt(search_area / params["max_image_sample_size"])
+    else:
+        if  search_area < params["min_image_sample_size"]:
+            currentScaleFactor = sqrt(search_area / params["min_image_sample_size"])
+        else:
+            currentScaleFactor = 1.0
+
+    # target size at the initial scale
+    base_target_sz = np.array(target_sz, float) / currentScaleFactor
+
+    # window size, taking padding into account
+    if params["search_area_shape"] == 'proportional':
+        img_sample_sz = math.floor( base_target_sz * params["search_area_scale"])

be4e40f686d9941d5afcac8a549b0c86bedd915d
JuliaKamelina <juliak.d@mail.ru>
2018-08-12 00:57:38 +0300

    some more

--- implementation/tracker.py
+++ implementation/tracker.py
@@ -36,6 +36,7 @@ def tracker(**params):
 
     #Define data types
     if params["use_gpu"]:
+        # TODO:
         params["data_type"] = pycuda.gpuarray.zeros(...)
     else:
         params["data_type"] = 0
@@ -50,17 +51,17 @@ def tracker(**params):
         is_color_image = True
     else:
         is_color_image = False
-        
+
     # Check if mexResize available
     # TODO:
 
     # Calculate search area and initial scale factor
     search_area = np.prod(np.array(init_target_sz, float) * params["search_area_scale"])
     if search_area > params["max_image_sample_size"]:
-        currentScaleFactor = sqrt(search_area / params["max_image_sample_size"])
+        currentScaleFactor = math.sqrt(search_area / params["max_image_sample_size"])
     else:
         if  search_area < params["min_image_sample_size"]:
-            currentScaleFactor = sqrt(search_area / params["min_image_sample_size"])
+            currentScaleFactor = math.sqrt(search_area / params["min_image_sample_size"])
         else:
             currentScaleFactor = 1.0
 
@@ -69,4 +70,31 @@ def tracker(**params):
 
     # window size, taking padding into account
     if params["search_area_shape"] == 'proportional':
-        img_sample_sz = math.floor( base_target_sz * params["search_area_scale"])
+        img_sample_sz = math.floor(base_target_sz * params["search_area_scale"])
+    if params["search_area_shape"] == 'square':
+        img_sample_sz = np.tile(math.sqtr(np.prod(base_target_sz*params["search_area_scale"])), 1, 2)[0]
+    if params["search_area_shape"] == 'fix_padding':
+        img_sample_sz = base_target_sz + math.sqrt(np.prod(base_target_sz*params["search_area_scale"]) + (base_target_sz[0] - base_target_sz[1])/4) - sum(base_target_sz)/2
+    if params["search_area_shape"] == 'custom':
+        img_sample_sz = np.array((base_target_sz[0]*2, base_target_sz[1]*2), float)
+
+    # TODO:
+    features, global_fparams, feature_info = init_features(features, global_fparams, is_color_image, img_sample_sz, 'odd_cells')
+
+    # Set feature info
+    img_support_sz = feature_info["img_support_sz"]
+    feature_sz = feature_info["data_sz"]
+    feature_dim = feature_info["dim"]
+    num_feature_blocks = len(feature_dim)
+
+    # Get feature specific parameters
+    # TODO:
+    feature_params = init_feature_params(features, feature_info)
+    feature_extract_info = get_feature_extract_info(features)
+
+    if params["use_projection_matrix"]:
+        sample_dim = feature_params["compressed_dim"]
+    else:
+        sample_dim = feature_dim
+
+    

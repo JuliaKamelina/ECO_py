99341b79ea5a4bc05e31af32a154b273e8c439a7
jkamelin <julia.kamelina@intel.com>
2020-05-11 01:00:10 +0300

    update load_info

--- implementation/utils/load_video_info.py
+++ implementation/utils/load_video_info.py
@@ -2,7 +2,8 @@ import numpy as np
 import os.path
 
 def load_video_info(path):
-    gt_path = "{}/groundtruth_rect.txt".format(path)
+    gt_path = "/".join(path.split("/")[:-2])
+    gt_path = "{}/groundtruth_rect.txt".format(gt_path)
     try:
         ground_truth = np.loadtxt(gt_path)
     except:
@@ -13,7 +14,7 @@ def load_video_info(path):
     seq["len"] = len(ground_truth)
     seq["init_rect"] = ground_truth[0]
 
-    img_path = path + '/img/'
+    img_path = path  # + '/img/'
     img_files = list()
     if (os.path.isfile(img_path + '%04d' % 1 + '.png')):
         for i in range(1,seq["len"]):
@@ -21,6 +22,22 @@ def load_video_info(path):
     elif (os.path.isfile(img_path + '%04d' % 1 + '.jpg')):
         for i in range(1,seq["len"]):
             img_files.append(img_path + '%04d' % i + '.jpg')
+    elif (os.path.isfile(img_path + '%05d' % 1 + '.jpg')):
+        for i in range(1,seq["len"]):
+            img_files.append(img_path + '%05d' % i + '.jpg')
+
+    elif (os.path.isfile(img_path + '%04d' % 3 + '.jpg')):
+        for i in range(3,seq["len"]):
+            img_files.append(img_path + '%04d' % i + '.jpg')
+
+    elif (os.path.isfile(img_path + '%04d' % 247 + '.jpg')):
+        for i in range(247,seq["len"]):
+            img_files.append(img_path + '%04d' % i + '.jpg')
+
+    elif (os.path.isfile(img_path + '%04d' % 18 + '.jpg')):
+        for i in range(18,seq["len"]):
+            img_files.append(img_path + '%04d' % i + '.jpg')
+
     elif (os.path.isfile(img_path + '%04d' % 1 + '.bmp')):
         for i in range(1,seq["len"]):
             img_files.append(img_path + '%04d' % i + '.bmp')

6366b666488ccf2ccc18d3d5364b2f05357ae717
jkamelin <julia.kamelina@intel.com>
2021-04-22 20:24:53 +0300

    add mu implementation

--- implementation/__init__.py
+++ implementation/__init__.py
@@ -1,7 +1,9 @@
-from .tracker import Tracker
+from .eco_tracker import ECOTracker
+from .eco_mu_tracker import ECOMUTracker
 from .dim_reduction import *
 from .optimize_scores import optimize_scores
 from .sample_space_model import SampleSpace
 from .train import train_joint, train_filter
 from .fourier_tools import *
 from .scale_filter import ScaleFilter
+from .pytracking import ltr
